25579cead21a83042b162075faf2ecdb4199adc9
Deri Wijaya <dericwijaya90@gmail.com>
2025-02-25 13:43:51 +0700

    Menambahkan beberapa file baru ke folder TUGAS

--- TUGAS/variable1.js
+++ TUGAS/variable1.js
@@ -1,5 +1,5 @@
 let  year  =  2006;
-console.log(year);  //  ->  2007
+console.log(year);  //  ->  2006
 console.log(1945);  //  ->  1945
-console.log("Derry");  //  ->  Nabil
+console.log("Derry");  //  ->  Derry
 
